746397b8eff996e878e1412090e56e1585f59dc4
JDGTS <46120947+JDGTS@users.noreply.github.com>
2018-12-27 19:55:46 +0800

    WinNet
    Windows Network Diagnostic Script

---
+++ WinNet.bat
@@ -0,0 +1,8 @@
+echo Network Config Info: >> C:\Users\Administrator\Desktop\TEST.txt
+ipconfig >> C:\Users\Administrator\Desktop\TEST.txt
+echo *************************************************************** >> C:\Users\Administrator\Desktop\TEST.txt
+echo Ping Test Result: >> C:\Users\Administrator\Desktop\TEST.txt
+ping www.baidu.com >> C:\Users\Administrator\Desktop\TEST.txt
+echo *************************************************************** >> C:\Users\Administrator\Desktop\TEST.txt
+echo Network Route Info: >> C:\Users\Administrator\Desktop\TEST.txt
+route print >> C:\Users\Administrator\Desktop\TEST.txt
